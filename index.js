8c25cfc10f19de2bbadd671bbd10c4609dcd7d66
CamTheBeast <75223349+CamTheBeast@users.noreply.github.com>
2024-12-24 21:46:01 -0800

    fixed

--- index.js
+++ index.js
@@ -3,7 +3,16 @@ window.onload = function () {
 
   // Wait for the audio to be fully loaded and ready to play
   music.addEventListener("loadeddata", function () {
+    console.log(
+      "Audio loaded. Current time before playing: " + music.currentTime
+    ); // Log before setting currentTime
+
     music.currentTime = 30; // Start at 30 seconds
+
+    console.log(
+      "Current time after setting to 30 seconds: " + music.currentTime
+    ); // Log after setting currentTime
+
     music.play(); // Ensure the audio plays
   });
 };
