9e17744277405584e864c560edb515a40675f9c5
CamTheBeast <75223349+CamTheBeast@users.noreply.github.com>
2024-12-24 21:32:02 -0800

    fized audio

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ window.onload = function () {
   // Wait for the audio to load
   music.oncanplaythrough = function () {
     music.currentTime = 59; // Start the song at 59 seconds
-    music.muted = false; // Unmute the audio
     music.play(); // Play the song automatically
   };
 };
