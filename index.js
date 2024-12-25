8e73d0be5791b172f79040306397be8b6306f18a
CamTheBeast <75223349+CamTheBeast@users.noreply.github.com>
2024-12-24 21:33:45 -0800

    fixed audio

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ window.onload = function () {
   // Wait for the audio to load
   music.oncanplaythrough = function () {
     music.currentTime = 59; // Start the song at 59 seconds
+    music.muted = false; // Unmute the audio
     music.play(); // Play the song automatically
   };
 };
