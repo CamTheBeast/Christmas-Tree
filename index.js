7eb3996cad6847d7410346590c57fec236c6f73e
CamTheBeast <75223349+CamTheBeast@users.noreply.github.com>
2024-12-24 21:44:53 -0800

    fixed

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 window.onload = function () {
   const music = document.getElementById("backgroundMusic");
 
-  // Wait for the audio to load
-  music.oncanplaythrough = function () {
-    music.currentTime = 30; // Start the song at 30 seconds
-    music.muted = false; // Unmute the audio
-    music.play(); // Play the song automatically
-  };
+  // Wait for the audio to be fully loaded and ready to play
+  music.addEventListener("loadeddata", function () {
+    music.currentTime = 30; // Start at 30 seconds
+    music.play(); // Ensure the audio plays
+  });
 };
